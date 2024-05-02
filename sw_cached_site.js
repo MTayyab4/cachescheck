cf8bfa7fc2c5caf2cf75cb41a95b5da0dcf0d355
MTayyab4 <126855912+MTayyab4@users.noreply.github.com>
2024-05-02 12:06:40 +0800

    sw_cached_site.js

--- sw_cached_site.js
+++ sw_cached_site.js
@@ -1,49 +1,43 @@
-const cacheName = 'v2';
+const cacheName = 'v1';
+const cacheAssets = [
+'index.html',
+'style.css',
+'main.js'
+];
 
-
-// Call Install Event
 self.addEventListener('install', e => {
-    console.log('Service Worker: Installed');
-
+console.log('Service Worker: Installed');
+e.waitUntil(
+caches.open(cacheName)
+.then(cache => {
+console.log("Service Worker: Caching Files");
+return cache.addAll(cacheAssets);
+})
+.then(() => self.skipWaiting())
+);
 });
 
-// Call Activate Event
 const version = 'v1';
 
 self.addEventListener('activate', e => {
-    console.log('Service Worker: Activated');
-    e.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(cache => {
-                    if (cache !== version) {
-                        console.log("Service Worker: Clearing old cache");
-                        return caches.delete(cache);
-                    }
-                }
-                )
-            )
-        })
-    );
+console.log('Service Worker: Activated');
+e.waitUntil(
+caches.keys().then(keyList => {
+return Promise.all(
+keyList.map(cache => {
+if (cache !== version) {
+console.log("Service Worker: Clearing old cache");
+return caches.delete(cache);
+}
+})
+);
+})
+);
 });
 
-//Call Fetch Event
 self.addEventListener('fetch', e => {
-    console.log('service worker : fetcheing');
-    e.respondWith(
-        fetch(e.request)
-            .then(res => {
-                // Make copy/clone of response
-                const resClone = res.clone()
-                //Open cache
-                caches.open(cacheName)
-                    .then(cache => {
-                        //Add response to cache
-                        cache.put(e.request, resClone);
-                    })
-                return res;
-            }).catch(err => caches.match(e.request).then(res => res))
-    );
-
-
-})
+console.log('service worker : fetching');
+e.respondWith(
+fetch(e.request).catch(() => caches.match(e.request))
+);
+});
